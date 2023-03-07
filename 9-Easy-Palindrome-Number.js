b4a1f76925c2e00efbe1b6552bb63214fc9f6eb2
Tu Dinh <tudinhacoustic@gmail.com>
2023-03-07 23:13:33 +0700

    [03/07/2023] - Solved Palindrome-Number

---
+++ 9-Easy-Palindrome-Number.js
@@ -0,0 +1,39 @@
+// const x = 121;
+// const isPalindrome = function (x) {
+//   if (x < 0 || (x % 10 == 0 && x != 0)) {
+//     return false;
+//   }
+//   if (x % parseInt(x.toString()[0]) == 0) {
+//     if (x.toString()[0] == 1) {
+//       if ((x * 2) % 2 == 2) {
+//         return true;
+//       }
+//       return false;
+//     }
+//     return true;
+//   }
+//   let revertedNumber = 0;
+//   while (x > revertedNumber) {
+//     revertedNumber = revertedNumber * 10 + (x % 10);
+//     x = x / 10;
+//   }
+//   return parseInt(x) == parseInt(revertedNumber) || x == revertedNumber / 10;
+// };
+// const result = isPalindrome(x);
+// console.log(result);
+
+const x = 1001;
+var isPalindrome = function (x) {
+  if (x < 0 || (x % 10 == 0 && x != 0)) {
+    return false;
+  }
+  let revertedNumber = 0;
+  while (x > revertedNumber) {
+    revertedNumber = revertedNumber * 10 + (x % 10);
+    x = parseInt(x / 10);
+  }
+  return x == revertedNumber || x == parseInt(revertedNumber / 10);
+};
+
+const result = isPalindrome(x);
+console.log(result);
